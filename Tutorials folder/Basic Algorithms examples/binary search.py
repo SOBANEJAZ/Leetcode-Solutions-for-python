37c791f0915a4cdae63f7bb5a4cfe1102934369f
Soban <sobanpythonista@gmail.com>
2023-07-12 09:29:27 +0500

    Update binary search.py

--- Tutorials folder/Basic Algorithms examples/binary search.py
+++ Tutorials folder/Basic Algorithms examples/binary search.py
@@ -4,3 +4,11 @@ def binary_search(list, target):
 
     while first <= last:
         midpoint = (first + last)//2
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint -1
+
+ 
